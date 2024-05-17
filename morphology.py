182ec2eaf564086ba06cd3995b71194e62611d6e
Roshana <roshana10@DESKTOP-JF840RJ>
2024-05-17 13:46:47 +0200

    morphology file

---
+++ morphology.py
@@ -0,0 +1,32 @@
+# Laura de Boer, Dertje Roggeveen, Roshana Vegter
+# Group 2
+# morphology.py
+
+import json
+
+def stemming():
+    pass
+
+
+def lemmatization():
+    pass
+
+
+def tokenization():
+    pass
+
+
+def stop_words():
+    pass
+
+
+def spelling_correction():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
