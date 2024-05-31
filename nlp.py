5f42027ad68e6d39f8b27ed47315b43bd7a1a9de
DertjeR <d.j.roggeveen@student.rug.nl>
2024-05-31 18:18:16 +0200

    updated syntax and main function

--- nlp.py
+++ nlp.py
@@ -50,27 +50,28 @@ def tokenization(text):
     return [token.text.strip() for token in text]
 
 
-def syntax(text):
-    with open('syntax.txt', 'w', encoding='utf-8') as syntax_file:
-        common_chunks = Counter(get_chunks(text))
-        syntax_file.write("10 Most Common Noun Chunks in text:\n")
-        for chunk, frequency in common_chunks.most_common(10):
-            syntax_file.write(f"{chunk}: {frequency}\n")
+def syntax(text_ai, syntax_file):
+    common_chunks_ai = Counter(get_chunks(text_ai))
+    syntax_file.write("10 Most Common Noun Chunks in text:\n")
+    for chunk, frequency in common_chunks_ai.most_common(10):
+        syntax_file.write(f"{chunk}: {frequency}\n")
 
-        common_proper_noun_chunks = Counter(get_chunks(text, filter_root='PROPN'))
-        syntax_file.write("\n10 Most Common Noun Chunks with Proper Noun Roots in text:\n")
-        for chunk, frequency in common_proper_noun_chunks.most_common(10):
-            syntax_file.write(f"{chunk}: {frequency}\n")
+    common_proper_noun_chunks_ai = Counter(get_chunks(text_ai, filter_root='PROPN'))
+    syntax_file.write("\n10 Most Common Noun Chunks with Proper Noun Roots in text:\n")
+    for chunk, frequency in common_proper_noun_chunks_ai.most_common(10):
+        syntax_file.write(f"{chunk}: {frequency}\n")
 
-        tags = []
-        for word in text:
-            tags.append(word.pos_)
-        syntax_file.write("10 Most frequent POS tags in text: " +
-                          str(count_tags(tags)) + "\n")
+    tags_ai = []
+    for word in text_ai:
+        tags_ai.append(word.pos_)
+    syntax_file.write("10 Most frequent POS tags in text: " +
+                      str(count_tags(tags_ai)) + "\n")
 
-        syntax_file.write("Most common specified dependency in text: " +
-                          str(most_frequent_asked_dependency(text, 'ADJ')) + "\n")
+    syntax_file.write("Most common specified dependency in text: " +
+                      str(most_frequent_asked_dependency(text_ai, 'ADJ')) + "\n")
 
+    syntax_file.write("----------------------------------------------------------\n")
+    syntax_file.write("\n")
 
 def semantics(text):
     pass
@@ -90,8 +91,11 @@ def main():
     ai_lemmas = lemmatization(ai_text)
     human_lemmas = lemmatization(human_text)
 
-    syntax(human_text)
-    syntax(ai_text)
+    with open('syntax.txt', 'w', encoding='utf-8') as syntax_file:
+        syntax_file.write("Artificial articles\n")
+        syntax(ai_text, syntax_file)
+        syntax_file.write("Human articles\n")
+        syntax(human_text, syntax_file)
 
     semantics(human_text)
     semantics(ai_text)
