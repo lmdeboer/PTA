4311ded3246a067773b109efa804621c6b51fdd1
Roshana <roshana10@DESKTOP-JF840RJ>
2024-06-01 12:12:38 +0200

    updated file for file structure

--- semantics.py
+++ semantics.py
@@ -1,49 +1,11 @@
-import nltk
+# Laura de Boer, Dertje Roggeveen, Roshana Vegter, Julian Paagman
+# Group 2
+# syntax.py
+
+
 from nltk.corpus import wordnet as wn
 from nltk.wsd import lesk
-import spacy
-import json
 from collections import Counter
-
-
-def read_file(file_name):
-    """
-    Function which reads the json file, and appends each article to the list texts, as well as apply a spacy object
-    on the document.
-    :param file_name: Json file to be read
-    :return: text as a spacy object.
-    """
-    texts = []
-    with open(file_name, 'r', encoding='utf-8') as file:
-        lines = file.readlines()
-        # half of the files, could not process more due to character limit
-        for line in lines[:326]:
-            json_obj = json.loads(line.strip())
-            text = json_obj.get('text', '')
-            texts.append(text)
-
-    all_text = ' '.join(texts)
-    nlp = spacy.load("en_core_web_sm")
-    doc = nlp(all_text)
-    return doc
-
-
-def lemmatization(text):
-    """
-    Function which lemmatizes the text.
-    :param text: Text to be lemmatized.
-    :return: Lemmatized text.
-    """
-    return [token.lemma_ for token in text]
-
-
-def tokenization(text):
-    """
-    Function which tokenizes the text.
-    :param text: Text to be tokenized.
-    :return: Tokenized text.
-    """
-    return [token.text.strip() for token in text]
 
 
 def unique_synsets(tokens):
@@ -123,59 +85,3 @@ def count_tokens_without_synsets(tokens):
     total_count = num_tokens_without_synsets
     common_tokens = Counter(tokens_without_synsets).most_common(10)
     return total_count, common_tokens
-
-
-def main():
-    nltk.download('wordnet')
-    nlp = spacy.load("en_core_web_sm")
-    
-    # Files
-    human_file = 'human.jsonl'
-    ai_file = 'group2.jsonl'
-    
-    # Process human articles
-    human_text = read_file(human_file)
-    human_lemmas = lemmatization(human_text)
-    human_tokens = tokenization(human_text)
-    
-    # Analyze human articles
-    human_ambiguous_words = count_ambiguous_words(human_tokens)
-    human_unique_synsets = unique_synsets(human_tokens)
-    human_hypernyms = get_noun_hypernyms(human_tokens)
-    human_most_common_hypernyms = common_hypernyms(human_hypernyms)
-    num_human_nouns_without_synsets, human_nouns_without_synsets = count_tokens_without_synsets(human_tokens)
-    
-    # Process AI articles
-    ai_text = read_file(ai_file)
-    ai_lemmas = lemmatization(ai_text)
-    ai_tokens = tokenization(ai_text)
-    
-    # Analyze AI articles
-    ai_ambiguous_words = count_ambiguous_words(ai_tokens)
-    ai_unique_synsets = unique_synsets(ai_tokens)
-    ai_hypernyms = get_noun_hypernyms(ai_tokens)
-    ai_most_common_hypernyms = common_hypernyms(ai_hypernyms)
-    num_ai_nouns_without_synsets, ai_nouns_without_synsets = count_tokens_without_synsets(ai_tokens)
-
-
-    with open('semantics.txt', 'w', encoding='utf-8') as semantics_file:
-        # Wite human article results to file
-        semantics_file.write("Human articles\n")
-        semantics_file.write(f"Number of ambiguous words: {human_ambiguous_words}\n")
-        semantics_file.write(f"Number of unique synsets: {human_unique_synsets}\n")
-        semantics_file.write(f"10 most common hypernyms in human articles: {human_most_common_hypernyms}\n")
-        semantics_file.write(f"Amount of tokens in human articles that have no synsets: {num_human_nouns_without_synsets}\n")
-        semantics_file.write(f"Top 10 tokens without synsets in human articles: {human_nouns_without_synsets}\n")
-        semantics_file.write("----------------------------------------------------------\n")
-        
-        # Write AI articles results to file.
-        semantics_file.write("AI generated articles\n")
-        semantics_file.write(f"Number of ambiguous words: {ai_ambiguous_words}\n")
-        semantics_file.write(f"Number of unique synsets: {ai_unique_synsets}\n")
-        semantics_file.write(f"10 most common hypernyms in AI text: {ai_most_common_hypernyms}\n")
-        semantics_file.write(f"Amount of tokens in AI articles that have no synsets: {num_ai_nouns_without_synsets}\n")
-        semantics_file.write(f"Top 10 tokens without synsets in AI articles: {ai_nouns_without_synsets}\n")
-        semantics_file.write("----------------------------------------------------------\n")
-
-if __name__ == '__main__':
-    main()
